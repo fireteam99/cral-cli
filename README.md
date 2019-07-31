546a385e2b61a1b312623ebc8227833ba2cdd7d2
fireteam99 <raymancheungsy@gmail.com>
2019-07-31 01:07:21 -0400

    finished prompts

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project_CRAL
-Helps you register for those pesky classes that fill up too fast for Rutgers University.
--- node/registration.js
+++ node/registration.js
@@ -28,7 +28,7 @@ const configQuestions = [
         message: 'Enter your netID username...'
     },
     {
-        type: 'input',
+        type: 'password',
         name: 'password',
         message: 'Enter your netID password...'
     },
@@ -43,13 +43,56 @@ const configQuestions = [
     {
         type: 'list',
         name: 'term',
-        message: 'Enter the term you would liked to register for...',
+        message: 'Choose the term you would liked to register for...',
         choices: [
-            'winter',
-            'spring',
-            'summer',
-            'fall'
-        ]
+            'Winter',
+            'Spring',
+            'Summer',
+            'Fall'
+        ],
+        filter: (v) => {
+            switch(v) {
+                case "Winter":
+                    return 0;
+                case "Spring":
+                    return 1;
+                case "Summer":
+                    return 7;
+                case "Fall":
+                    return 9;
+                default:
+                    return -1;
+            }
+        }
+    },
+    {
+        type: 'number',
+        name: 'timeout',
+        message: `Enter a delay in seconds
+            Rutgers SOC API. Checking too often may be considered suspicious
+            and could result in a ban. Defaults to 180 seconds (2 minutes).`,
+        default: 180,
+        validate: (v) => {
+            if (v > 0) {
+                return true;
+            } else {
+                return 'Please enter a non negative number of seconds.'
+            }
+        }
+    },
+    {
+        type: 'number',
+        name: 'randomization',
+        message: `Enter a maximum randomization amount in seconds for the
+            timeout as a disguise. Defaults to 10 seconds.`,
+        default: 10,
+        validate: (v) => {
+            if (v > 0) {
+                return true;
+            } else {
+                return 'Please enter a non negative number of seconds.'
+            }
+        }
     }
 ];
 
