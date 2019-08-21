57fc3461a9ef1bfc7ba5248c808f2b57d3d7fd08
fireteam99 <raymancheungsy@gmail.com>
2019-08-21 00:53:07 -0400

    fixed and tested milToStdTime util func

--- util/militaryToStandardTime.js
+++ util/militaryToStandardTime.js
@@ -1,16 +1,18 @@
-module.exports = mTime => {
+const milToStd = mTime => {
     if (mTime == null) {
         return 'N/A';
     }
     const millyTime = Number(mTime);
     if (millyTime < 100) {
-        return `12:${millyTime} AM`;
+        const digits = millyTime.toString().split('');
+        return digits.length > 1 ? `12:${millyTime} AM` : `12:0${millyTime} AM`;
     } else if (millyTime < 1200) {
         const digits = millyTime.toString().split('');
         return digits.length < 4
-            ? `${digits[0]}:${digits[1]}${digits[2]} PM`
-            : `${digits[0]}${digits[1]}:${digits[2]}${digits[3]} PM`;
+            ? `${digits[0]}:${digits[1]}${digits[2]} AM`
+            : `${digits[0]}${digits[1]}:${digits[2]}${digits[3]} AM`;
     } else if (millyTime >= 1200 && millyTime < 1300) {
+        const digits = millyTime.toString().split('');
         return `${digits[0]}${digits[1]}:${digits[2]}${digits[3]} PM`;
     } else {
         const digits = (millyTime - 1200).toString().split('');
@@ -19,3 +21,10 @@ module.exports = mTime => {
             : `${digits[0]}${digits[1]}:${digits[2]}${digits[3]} PM`;
     }
 };
+
+module.exports = milToStd;
+
+// tests to make sure it words
+// for (let i = 0; i < 2400; i++) {
+//     console.log(milToStd(i));
+// }
